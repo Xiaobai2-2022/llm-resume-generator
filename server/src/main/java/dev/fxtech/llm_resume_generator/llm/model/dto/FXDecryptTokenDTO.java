0eb2c4744acfa4159b6a0da76e300e9810ca039c
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2025-10-18 16:09:18 -0400

    Fixed Formatting Error

--- server/src/main/java/dev/fxtech/llm_resume_generator/llm/model/dto/FXDecryptTokenDTO.java
+++ server/src/main/java/dev/fxtech/llm_resume_generator/llm/model/dto/FXDecryptTokenDTO.java
@@ -11,14 +11,14 @@ public record FXDecryptTokenDTO(
     FXTokenCryptoUtil.FXEncryptedToken encryptedToken
 ) {
     public FXDecryptTokenDTO {
-            if(!StringUtils.hasText(id)) {
-                throw new FXException(FXErrno.USERID_MISSING, "ID does not exist in FXDecryptTokenDTO");
-            }
-            if(!StringUtils.hasText(createdBy)) {
-                throw new FXException(FXErrno.USERNAME_MISSING, "Name does not exist in FXDecryptTokenDTO");
-            }
-            if(encryptedToken == null) {
+        if(!StringUtils.hasText(id)) {
+            throw new FXException(FXErrno.USERID_MISSING, "ID does not exist in FXDecryptTokenDTO");
+        }
+        if(!StringUtils.hasText(createdBy)) {
+            throw new FXException(FXErrno.USERNAME_MISSING, "Name does not exist in FXDecryptTokenDTO");
+        }
+        if(encryptedToken == null) {
             throw new FXException(FXErrno.TOKEN_MISSING, "Token does not exist in FXDecryptTokenDTO");
-            }
         }
     }
+}
